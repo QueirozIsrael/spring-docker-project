699209d87758603d21e92a8eb5d410f5d2f4b00f
Israel Queiroz <israel.qoliveira@gmail.com>
2024-09-28 18:07:02 -0300

    mapeando os objetos de resposta

--- src/main/java/services/PropostaService.java
+++ src/main/java/services/PropostaService.java
@@ -1,5 +1,6 @@
 package services;
 
+import Mapper.PropostaMapper;
 import dto.PropostaRequestDto;
 import dto.PropostaResponseDto;
 import entities.Proposta;
@@ -14,7 +15,9 @@ public class PropostaService {
     private PropostaRepository propostaRepository;
 
     public PropostaResponseDto criar(PropostaRequestDto requestDto){
-        propostaRepository.save(new Proposta());
-        return null;
+        Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
+        propostaRepository.save(proposta);
+
+        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
     }
 }
